35a4fed6a4ed9e533a75566241aa9f5206647a48
Pallarco-Jose <132375593+Pallarco-Jose@users.noreply.github.com>
2023-05-14 21:42:48 -0500

    Creacion de menus

--- Assets/Scripts/CustomNetworkManager.cs
+++ Assets/Scripts/CustomNetworkManager.cs
@@ -9,32 +9,32 @@ public class CustomNetworkManager : NetworkManager
     {
         base.OnClientConnect();
     }
-    //public override void OnServerAddPlayer(NetworkConnectionToClient conn)
-    //{
-    //    base.OnServerAddPlayer(conn);
+    public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+    {
+        base.OnServerAddPlayer(conn);
 
-    //    //Debug.Log("numero de players en cnm: " + numPlayers);
+        //Debug.Log("numero de players en cnm: " + numPlayers);
 
-    //    //GameManager gm = GameObject.Find("GameManager").GetComponent<GameManager>();
-    //    //gm.UpdateConectedPlayers(numPlayers);
+        //GameManager gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+        //gm.UpdateConectedPlayers(numPlayers);
 
 
 
-    //   // GameObject player = Instantiate(playerPrefab);
-    //    //NetworkServer.AddPlayerForConnection(conn, player);
+        // GameObject player = Instantiate(playerPrefab);
+        //NetworkServer.AddPlayerForConnection(conn, player);
 
-    //    // Llenar el array de jugadores en el GameManager
-    //   // PlayerManager playerManager = player.GetComponent<PlayerManager>();
+        // Llenar el array de jugadores en el GameManager
+        // PlayerManager playerManager = player.GetComponent<PlayerManager>();
 
-    //    PlayerManager playerManager = conn.identity.GetComponent<PlayerManager>();
-    //    if (numPlayers == 1)
-    //    {   
-    //        playerManager.isPlayerTurn = true;
-    //    }
-    //    GameManager.Instance.AddPlayer(playerManager);
+        PlayerManager playerManager = conn.identity.GetComponent<PlayerManager>();
+        if (numPlayers == 1)
+        {
+            playerManager.isPlayerTurn = true;
+        }
+        GameManager.Instance.AddPlayer(playerManager);
 
-    //    GameManager.Instance.UpdateConectedPlayers(numPlayers);
+        GameManager.Instance.UpdateConectedPlayers(numPlayers);
 
-    //}
+    }
 
 }
